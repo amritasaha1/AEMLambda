8c9ceca2cc09cc6a274d54b09f0a21ad1ad1c425
amritasaha1 <37227954+amritasaha1@users.noreply.github.com>
2018-03-11 18:05:48 -0500

    Service Integration
    Commit After Integration

--- src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -1,7 +1,13 @@
 package com.amazonaws.lambda.demo;
 
 import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.ResponseEntity;
+
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class LambdaFunctionHandler implements RequestHandler<Map<String, Object>, Object> {
@@ -10,9 +16,36 @@ public class LambdaFunctionHandler implements RequestHandler<Map<String, Object>
     public Object handleRequest(Map<String,Object> input, Context context) {
        LexRequest lexRequest = (LexRequest) LexRequestFactory.createLexRequest(input);
        //service call
-       Message message = new Message("PlainText","We will get back soon. We noted your credit score");
+       LambdaLogger logger = context.getLogger();
+       String aemOutput = callAEMServicefor(lexRequest.getBotName(), logger);
+          
+       Message message = new Message("PlainText",aemOutput);
        DialogAction dialogAction = new DialogAction("Close","Fulfilled",message);
        return new LexResponse(dialogAction);
     }
+    
+    
+    private String callAEMServicefor(String userInput, LambdaLogger logger) {
+        System.out.println("You are here in service called method.....");
+        String serviceresponse = "This is mock service response....";
+        BasicAuthRestTemplate restTemplate = new BasicAuthRestTemplate("summituser", "abcd");
+        ResponseEntity<String> jsonresult = restTemplate.getForEntity("http://54.195.246.137/bin/trainingServlet?query=cards", String.class);
+        JSONObject jsonAEMResponse = new JSONObject(jsonresult);
+        //logger.log("Full response details:: " + jsonAEMResponse.toString());
+        String responseBody = (String)jsonAEMResponse.get("body");                    
+        JSONObject responseJSON = new JSONObject(responseBody);
+        serviceresponse = responseJSON.toString();
+        JSONArray cardArray = (JSONArray)responseJSON.get("creditcards");
+        StringBuffer creditCards = new StringBuffer();
+        for(int i=0;i<cardArray.length();i++) {
+                        String s = (String)cardArray.getJSONObject(i).get("product_name");
+                        creditCards.append(s).append(",");
+        }
+        logger.log("Available Credit Cards:: " +  creditCards.toString());
+        return serviceresponse;
+}
+
+    
+    
 
 }
